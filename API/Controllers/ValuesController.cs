3ae8966b64c3fe3c9b883525b43471417d26bbba
David Oh <DavidOh@protonmail.com>
2019-12-23 16:39:06 -0800

    Add ValuesController to API controller

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -30,5 +30,19 @@ namespace API.Controllers
         {
             
         }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
     }
 }
